0b72ecdd9bc9dd5f0ca5189ca8bad34be4b519f8
Kuljira Namjai <synthiawithsyren@gmail.com>
2026-03-25 11:59:17 +0700

    sdfsdfs

--- src/interfaces/response.interface.ts
+++ src/interfaces/response.interface.ts
@@ -1,13 +1,5 @@
 // ใช้เป็น response มาตรฐานทุก endpoint
 export interface ApiResponse<T> {
-  push(newPurchase: {
-    customerName: string;
-    purchaseDate: string;
-    items: [{ productId: number; quantity: number }];
-    id: string;
-  }): unknown;
-  length: number;
-  find(arg0: (u: any) => boolean): unknown;
   success: boolean;
   data: T | null;
   message: string;
